17f641b829e6f79eade08111128219b3d28f16d7
Francisco Lemos Caldas <francisco.caldas@outsystems.com>
2023-11-10 01:16:42 +0000

    modified 3 files in .

--- extension.js
+++ extension.js
@@ -1,90 +1,45 @@
 const vscode = require('vscode');
-const { spawn } = require('child_process');
 
-function activate(context) {
-    const subclassProvider = new SubclassProvider();
-    vscode.window.registerTreeDataProvider('subclassView', subclassProvider);
-
-    const getSubclassesCommand = vscode.commands.registerCommand('extension.getSubclasses', function (uri) {
-        subclassProvider.refresh();
-    });
-
-    context.subscriptions.push(getSubclassesCommand);
+class TreeItem {
+    constructor(label, collapsibleState) {
+        this.label = label;
+        this.collapsibleState = collapsibleState;
+    }
 }
 
-class SubclassProvider {
+class TreeDataProvider {
     constructor() {
         this._onDidChangeTreeData = new vscode.EventEmitter();
         this.onDidChangeTreeData = this._onDidChangeTreeData.event;
     }
 
-    refresh() {
-        this._onDidChangeTreeData.fire();
+    getChildren(element) {
+        if (!element) {
+            return ['Hello World'];
+        } else if (element === 'Hello World') {
+            return ['Child 1', 'Child 2', 'Child 3'];
+        } else {
+            return []; // No more children for individual items
+        }
     }
 
     getTreeItem(element) {
-        return element;
+        return new TreeItem(element, vscode.TreeItemCollapsibleState.Collapsed);
     }
 
-    async getChildren(element) {
-        if (!element) {
-            // Initialize the root nodes
-            return [new ClassNode('Animal', 'Animal')];
-        } else {
-            const className = element.className;
-            const subclasses = await findSubclasses(className);
-            return subclasses.map(subclass => new ClassNode(subclass, className));
-        }
+    refresh() {
+        this._onDidChangeTreeData.fire();
     }
 }
 
-class ClassNode extends vscode.TreeItem {
-    constructor(label, className) {
-        super(label, vscode.TreeItemCollapsibleState.Collapsed);
-        this.className = className;
-    }
-}
+function activate(context) {
+    const treeDataProvider = new TreeDataProvider();
+    vscode.window.registerTreeDataProvider('helloWorld', treeDataProvider);
 
-async function findSubclasses(className) {
-    try {
-        const document = vscode.window.activeTextEditor.document;
-        if (document.languageId === 'python') {
-            const text = document.getText();
-            const pythonProcess = spawn('python', ['-c', `
-                import ast
-
-                def find_subclasses(node, target_class):
-                    if isinstance(node, ast.ClassDef):
-                        if target_class in [base.id for base in node.bases]:
-                            yield node.name
-                    for child in ast.iter_child_nodes(node):
-                        yield from find_subclasses(child, target_class)
-
-                tree = ast.parse('''${text}''')
-                subclasses = list(find_subclasses(tree, '${className}'))
-                print(subclasses)
-            `], { shell: true });
-
-            return new Promise((resolve, reject) => {
-                let output = '';
-                pythonProcess.stdout.on('data', data => {
-                    output += data.toString();
-                });
-
-                pythonProcess.on('close', code => {
-                    if (code === 0) {
-                        resolve(output.trim().split('\n'));
-                    } else {
-                        reject(`Error executing Python script: ${output}`);
-                    }
-                });
-            });
-        }
-    } catch (error) {
-        console.error(error);
-    }
-
-    return [];
+    // Create a command to refresh the tree view
+    vscode.commands.registerCommand('extension.refreshTree', () => {
+        treeDataProvider.refresh();
+    });
 }
 
 function deactivate() {}
